10d327d270f81a7e19b49a4813e091934014e721
GiusGasSmart <giuseppe.gasbarro@smartshaped.com>
2025-03-10 12:00:05 +0100

    Fix method name for BinaryDownloader

--- chaM3Leon/src/main/java/com/smartshaped/chameleon/harvester/downloader/BinaryDownloader.java
+++ chaM3Leon/src/main/java/com/smartshaped/chameleon/harvester/downloader/BinaryDownloader.java
@@ -3,9 +3,6 @@ package com.smartshaped.chameleon.harvester.downloader;
 import static org.apache.spark.sql.functions.udf;
 import static org.apache.spark.sql.types.DataTypes.StringType;
 
-import com.smartshaped.chameleon.common.exception.ConfigurationException;
-import com.smartshaped.chameleon.harvester.exception.DownloaderException;
-import com.smartshaped.chameleon.harvester.request.Request;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -14,14 +11,23 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.sql.expressions.UserDefinedFunction;
+
+import com.smartshaped.chameleon.common.exception.ConfigurationException;
+import com.smartshaped.chameleon.harvester.exception.DownloaderException;
+import com.smartshaped.chameleon.harvester.request.Request;
 
 public abstract class BinaryDownloader extends Downloader {
   private static final Logger logger = LogManager.getLogger(BinaryDownloader.class);
@@ -55,7 +61,7 @@ public abstract class BinaryDownloader extends Downloader {
     Dataset<Row> uriFinal =
         uriDataset.withColumn("path", downloadBinaryFile.apply(uriDataset.col("value")));
 
-    uriFinal = extraProcessing(uriFinal, request);
+    uriFinal = addRequest(uriFinal, request);
     return uriFinal;
   }
 
@@ -111,7 +117,7 @@ public abstract class BinaryDownloader extends Downloader {
     }
   }
 
-  protected Dataset<Row> extraProcessing(Dataset<Row> dataset, Request request) {
+  protected Dataset<Row> addRequest(Dataset<Row> dataset, Request request) {
     dataset = dataset.withColumn("requestContent", functions.lit(request.getContent()));
     return dataset;
   }
